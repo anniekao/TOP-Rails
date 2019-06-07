e6ad1ca103d5161c9108662f104f2a04eecc532b
Annie <anniekao@posteo.net>
2019-06-07 17:09:16 -0400

    Sorcery authentication complete

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190607123148) do
+ActiveRecord::Schema.define(version: 20190607195709) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20190607123148) do
     t.string "image_content_type"
     t.string "image_file_size"
     t.string "image_updated_at"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["email"], name: "index_authors_on_email", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
